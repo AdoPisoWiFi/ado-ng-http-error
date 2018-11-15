7b67d38a6ca00b6f22c639d9dd7431ae25a52960
Adones Pitogo <pitogo.adones@gmail.com>
2018-11-15 14:34:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Extracts error message from http response
+
+This module provides `httpError` - function that extracts error string from http response object
+
+# Usage
+
+Module name: `ado-ng-http-error`
+Service name: `httpError`
+
+```javascript
+$http.get('/url').catch(function (res) {
+  var errStr = httpError(res);
+  console.log(errStr);
+});
+```
---
+++ ado-ng-http-error.js
@@ -0,0 +1,22 @@
+
+angular
+  .module('ado-ng-http-error', [])
+  .service('httpError', ['$q', function ($q) {
+
+    return function httpError(res) {
+      res.data = res.data || {};
+      var errMsg = 'Something went wrong';
+      if (typeof res.data == 'string')
+        return res.data;
+      else if(typeof res.data.error == 'string')
+        return res.data.error;
+      else if(typeof res.data.message == 'string')
+        return res.data.message;
+      else {
+        if (res.status === 413)
+          return 'File too large';
+      }
+      return errMsg;
+    };
+
+  }]);
